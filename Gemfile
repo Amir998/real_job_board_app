bec90819d5b31d5a18eacdb1defe3f80addf68e4
Amir Abaza <amir1b.6@gmail.com>
2019-05-25 14:38:06 +0200

    Styled forms 1/2

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'hirb'
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
+gem 'simple_form'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,10 @@
 class Job < ApplicationRecord
   has_many :companies, dependent: :destroy
   accepts_nested_attributes_for :companies, allow_destroy: true, reject_if: :all_blank
+
+  JOB_TYPE = ['Full-Time', 'Contract']
+
+  CATEGORY = ['Design', 'Programming', 'Customer Support',
+              'Copywriting', 'DevOps & Sysadmin', 'Sales & Marketing',
+              'Business & Management', 'Finance & Legal', 'Product', 'All Other']
 end
